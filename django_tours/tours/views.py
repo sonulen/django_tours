c3d983b896ce47b7e2bbb0d60fdb4c20ad88b616
sonulen <sonulen@gmail.com>
2020-03-22 18:31:13 +0300

    update: Сделал выборку данных по get запросу

--- django_tours/tours/views.py
+++ django_tours/tours/views.py
@@ -2,6 +2,7 @@ from django.http import (Http404, HttpResponse)
 from django.shortcuts import render
 from django.views import View
 
+from tours.data import *
 
 class MainPageView(View):
 
@@ -18,8 +19,16 @@ class TourPageView(View):
     template_path = "tours/tour.html"
 
     def get(self, request, id : int):
+        if id not in tours.keys():
+            # Не нашли тура с таким id
+            raise Http404
+        
         return render(
-            request, self.template_path
+            request, 
+            self.template_path, 
+            context={
+                'tour': tours[id],
+            }
         )
 
 
@@ -28,8 +37,26 @@ class DeparturePageView(View):
     template_path = "tours/departure.html"
 
     def get(self, request, departure : str):
+        if departure not in departures.keys():
+            # Такого направления нет
+            raise Http404
+
+        # Получим только туры по направлению
+        # Вопрос: почему filtered_tours 
+        # получается не отсортированный по ключам?
+        filtered_tours = {
+            key:tour for key, tour in tours.items() 
+            if tour["departure"] == departure
+            }
+
+
         return render(
-            request, self.template_path
+            request, 
+            self.template_path,
+            context={
+                'departure' : departure,
+                'tours' : filtered_tours
+            }
         )
 
 def custom_404(request, exception):
