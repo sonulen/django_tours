90090ba92787d94d2bc4dd08485e9ee458ed9508
sonulen <sonulen@gmail.com>
2020-03-22 18:09:28 +0300

    update: Настроил routes

--- django_tours/tours/views.py
+++ django_tours/tours/views.py
@@ -1,3 +1,36 @@
+from django.http import (Http404, HttpResponse)
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class MainPageView(View):
+
+    template_path = "main.html"
+
+    def get(self, request):
+        return render(
+            request, self.template_path
+        )
+
+
+class TourPageView(View):
+
+    template_path = "tours/tour.html"
+
+    def get(self, request, id : int):
+        return render(
+            request, self.template_path
+        )
+
+
+class DeparturePageView(View):
+
+    template_path = "tours/departure.html"
+
+    def get(self, request, departure : str):
+        return render(
+            request, self.template_path
+        )
+
+def custom_404(request, exception):
+    return render(request, '404.html')
