cc8504d6a4eba95e950e6eb41c1473260fce9f03
sonulen <sonulen@gmail.com>
2020-03-31 18:55:08 +0300

    update: Исправил согласно замечаниям после 2 главы

--- tours/templatetags/filters.py
+++ tours/templatetags/filters.py
@@ -1,20 +1,22 @@
 from django import template
-from django.urls import resolve
 
 register = template.Library()
+
 
 @register.filter
 def is_current_page(request, acronym) -> bool:
     return acronym in request.path
 
-@register.filter
-def min_by_key(dict, key: str) -> int:
-    # Сгенерируем список только из элементов слова по ключу key
-    list_ = [item[1][key] for item in dict.items()]
-    return min(list_) 
 
 @register.filter
-def max_by_key(dict, key: str) -> int:
+def min_by_key(dict_with_data, key: str) -> int:
     # Сгенерируем список только из элементов слова по ключу key
-    list_ = [item[1][key] for item in dict.items()]
-    return max(list_) 
+    items_list = [item[1][key] for item in dict_with_data.items()]
+    return min(items_list)
+
+
+@register.filter
+def max_by_key(dict_with_data, key: str) -> int:
+    # Сгенерируем список только из элементов слова по ключу key
+    items_list = [item[1][key] for item in dict_with_data.items()]
+    return max(items_list)
